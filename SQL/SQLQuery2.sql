7f4816b9fb41d1e51e87b11e51353f167ac61443
Yalman-Zeynep <yalmanzeynep72@gmail.com>
2024-06-27 16:48:47 +0300

    Create SQLQuery2.sql

---
+++ SQL/SQLQuery2.sql
@@ -0,0 +1,26 @@
+/*Normalizasyon tabloda veri tekrarini önler. daha duzenli daha planli olur. seviyeleri vardýr. en optimize olan ucuncu normal formdur
+
+
+birinci normal form
+sartlari: bir tabloda bir sutunde bir deðerden iki tane bulunamaz
+bir hücrede tek veri olur virgul bulunmaz 
+her tabloda bir benzersiz id olmalý ve tam baglanmalýdýr
+
+ikinci normal form
+gereksiz verileri kaldirmamýz gerekir ve bunlar ayri tablolara yerleþtirilir
+
+ucuncu normal form
+anahtar olmayan tum niteliklerin yalnizca primary keyde iþlevsel olmasini degil 
+ayni zamanda birbirinden bagimsiz olmasini da saglayarak bagimliligi kaldirir
+
+dorduncu normal form
+ Cok degerli bagimliliklri giderir. Bir kayittaki bir varlik hakkinda birden fazla bagýmsýz,
+ cok degerli gerçegin bulunmamasini saglar.
+
+besinci normal form
+form, bilgilerin daha kucuk, farkli düzenlenmis veri parcalarindan
+yeniden yapilandirilmasiyla ilgilidir.
+
+
+tablo uzerinden uygulama yapildi.
+
